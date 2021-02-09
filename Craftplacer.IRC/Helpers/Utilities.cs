ccc827ed6c715e73a685758c133174ddd291b4d5
Craftplacer <craftplacer@outlook.de>
2021-02-09 15:31:27 +0100

    Add SafeSequenceEqual

--- Craftplacer.IRC/Helpers/Utilities.cs
+++ Craftplacer.IRC/Helpers/Utilities.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Craftplacer.IRC.Helpers
 {
     public static class Utilities
     {
         internal static (string Nickname, string Username, string Host) ExtractHostmask(string hostmask) => throw new NotImplementedException();
+    
+        internal static bool SafeSequenceEqual<T>(IEnumerable<T> a, IEnumerable<T> b)
+        {
+            if (a == null)
+            {
+                return b == null;
+            }
+            else if (b == null)
+            {
+                return false;
+            }
+            else
+            {
+                return a.SequenceEqual(b);
+            }
+        }
     }
 }
