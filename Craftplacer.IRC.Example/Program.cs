f73aa85ac6caa6148e621e6d8e47094c6e4e7336
Craftplacer <craftplacer@outlook.de>
2021-02-09 15:57:37 +0100

    Add missing entry point

---
+++ Craftplacer.IRC.Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Craftplacer.IRC.Example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
