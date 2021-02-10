3ef6183d412ea2781b051dd9e4b35298499b8175
Craftplacer <craftplacer@outlook.de>
2021-02-10 10:18:07 +0100

    Finish base functionality of RawIrcClient

--- Craftplacer.IRC/Raw/RawIrcClient.cs
+++ Craftplacer.IRC/Raw/RawIrcClient.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Security;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
+
+using Craftplacer.IRC.Raw.Events;
+using Craftplacer.IRC.Raw.Messages;
 
 namespace Craftplacer.IRC.Raw
 {
-    public class RawIrcClient
+    // TODO: Look into async disposing
+    public class RawIrcClient : IDisposable
     {
         public RawIrcClient()
         {
@@ -17,20 +19,58 @@ namespace Craftplacer.IRC.Raw
 
         private readonly TcpClient tcp;
         private Stream stream;
+        private StreamWriter writer;
+        private StreamReader reader;
+        public event EventHandler<RawMessageReceivedEventArgs> MessageReceived;
 
-
-
-        public async Task ConnectAsync(string host, int port = 6697, bool ssl = false)
+        public async Task ConnectAsync(string host, int port, bool ssl = false)
         {
             await tcp.ConnectAsync(host, port);
 
             stream = tcp.GetStream();
 
-            // Wrap SSL around NetworkStream if user wants an encrypted connection.
+            // Wrap SSL around NetworkStream if the user wants an encrypted connection.
             if (ssl)
+            {
                 stream = new SslStream(stream);
+            }
+
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream);
+
+            _ = Task.Run(ListenAsync);
         }
 
+        private async Task ListenAsync()
+        {
+            while (tcp.Connected)
+            {
+                var line = await reader.ReadLineAsync();
 
+                if (line == null)
+                {
+                    break;
+                }
+
+                var message = RawMessage.Parse(line);
+
+                MessageReceived?.Invoke(this, new RawMessageReceivedEventArgs(this, message));
+            }
+        }
+
+        public async Task SendMessageAsync(RawMessage message)
+        {
+            var line = message.ToString();
+            await writer.WriteLineAsync(line);
+            await writer.FlushAsync();
+        }
+
+        public void Dispose()
+        {
+            writer?.Dispose();
+            reader?.Dispose();
+            stream?.Dispose();
+            tcp.Dispose();
+        }
     }
 }
