dc8749ae00528f75a2124d9526cc361c26e1175b
Craftplacer <craftplacer@outlook.de>
2021-02-11 02:13:48 +0100

    First commit of IrcClient

---
+++ Craftplacer.IRC/IrcClient.Handling.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using Craftplacer.IRC.Raw.Events;
+using Craftplacer.IRC.Raw.Messages;
+
+namespace Craftplacer.IRC
+{
+    public partial class IrcClient
+    {
+        private async void Raw_MessageReceived(object sender, RawMessageReceivedEventArgs e)
+        {
+            // Check if the received message is a server reply or a command.
+            if (ushort.TryParse(e.Message.Command, out var replyNum))
+            {
+                var reply = (ServerReply)replyNum;
+                switch (reply)
+                {
+                    case ServerReply.RPL_WELCOME:
+                    {
+                        Welcome?.Invoke(this, EventArgs.Empty);
+                        break;
+                    }
+
+                    case ServerReply.RPL_MOTD:
+                    {
+                        if (_motdBuffer != null)
+                        {
+                            _motdBuffer.AppendLine(e.Message.Parameters[0]);
+                        }
+                        break;
+                    }
+
+                    case ServerReply.RPL_MOTDSTART:
+                    {
+                        if (_motdBuffer == null)
+                        {
+                            _motdBuffer = new StringBuilder();
+                        }
+
+                        _motdBuffer.AppendLine(e.Message.Parameters[0]);
+                        break;
+                    }
+
+                    case ServerReply.RPL_MOTDEND:
+                    {
+                        Motd = _motdBuffer.ToString();
+                        _motdBuffer.Clear();
+                        break;
+                    }
+
+                    default:
+                    {
+                        Debug.WriteLine("Unhandled IRC server reply: {1} ({0}) {2}", e.Message.Command, reply, string.Join(' ', e.Message.Parameters));
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                switch (e.Message.Command)
+                {
+                    case "CAP":
+                    {
+                        var capCommand = e.Message.Parameters[1];
+                        switch (capCommand)
+                        {
+                            case "LS":
+                            {
+                                if (_negotiatingCapabilities)
+                                {
+                                    break;
+                                }
+
+                                _serverCapabilities = e.Message.Parameters[2].Split(' ');
+                                Debug.WriteLine("Offered capabilities: {0}", args: string.Join(", ", _serverCapabilities));
+
+                                var supportedCapabilities = _serverCapabilities.Where(sc => _supportedCapabilities.Contains(sc));
+
+                                if (supportedCapabilities.Any())
+                                {
+                                    _negotiatingCapabilities = true;
+                                    await Raw.SendMessageAsync(new RawMessage("CAP", "REQ", string.Join(' ', supportedCapabilities)));
+                                }
+                                else
+                                {
+                                    // We don't support any of the offered capabilities, we end here.
+                                    await Raw.SendMessageAsync(new RawMessage("CAP", "END"));
+                                    Debug.WriteLine("Negotiated no capabilities");
+                                }
+
+                                break;
+                            }
+
+                            case "ACK":
+                            {
+                                if (!_negotiatingCapabilities)
+                                {
+                                    break;
+                                }
+
+                                _acknowledgedCapabilities = e.Message.Parameters[2].Split(' ');
+                                await Raw.SendMessageAsync(new RawMessage("CAP", "END"));
+
+                                _negotiatingCapabilities = false;
+                                Debug.WriteLine("Finished negotiating capabilities: {0}", args: string.Join(", ", _acknowledgedCapabilities));
+
+                                break;
+                            }
+
+                            default:
+                            {
+                                Debug.WriteLine("Unhandled capability command: {0}", args: capCommand);
+
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+
+                    case "PING":
+                        e.Client.SendMessageAsync(new RawMessage("PONG", e.Message.Parameters[0]));
+                        break;
+
+                    default:
+                    {
+                        Debug.WriteLine("Unhandled IRC message: {0}", e.Message);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- Craftplacer.IRC/IrcClient.cs
+++ Craftplacer.IRC/IrcClient.cs
@@ -1,12 +1,148 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
+using Craftplacer.IRC.Raw;
+using Craftplacer.IRC.Raw.Messages;
+
 namespace Craftplacer.IRC
 {
-    public class IrcClient
+    /// <summary>
+    /// A "managed" IRC client. It wraps around RawIrcClient and provides a more user-friendly interface for the modern IRC protocol.
+    /// </summary>
+    // TODO: Require setting nick and real name.
+    public partial class IrcClient : IDisposable
     {
-        internal Task SendMessageAsync(string username, string message) => throw new NotImplementedException();
+        private static readonly string[] _supportedCapabilities = new string[]
+        {
+            "message-tags"
+        };
+
+        private string[] _acknowledgedCapabilities;
+        private StringBuilder _motdBuffer;
+        private bool _negotiatingCapabilities = false;
+        private string _nickname;
+        private string _password;
+        private string _realName;
+        private string[] _serverCapabilities;
+
+        public IrcClient()
+        {
+            Raw = new RawIrcClient();
+            Raw.MessageReceived += Raw_MessageReceived;
+        }
+
+        /// <summary>
+        /// The server welcomed the client and is willing to operate.
+        /// </summary>
+        public event EventHandler Welcome;
+
+        public bool Connected => Raw.Connected;
+
+        /// <summary>
+        /// The last "Message of the Day" sent by the server.
+        /// </summary>
+        public string Motd { get; private set; }
+
+        /// <summary>
+        /// The nickname of the client. Its value will be used when connecting to the server and can't be changed once connected.
+        /// </summary>
+        public string Nickname
+        {
+            get => _nickname;
+            set
+            {
+                if (Connected)
+                {
+                    throw new InvalidOperationException($"You can't change your nickname while connected. Use {nameof(ChangeNicknameAsync)}() instead.");
+                }
+
+                _nickname = value;
+            }
+        }
+
+        /// <summary>
+        /// The password required to authenticate with the server. Its value will be used when connecting to the server and can't be changed once connected.
+        /// </summary>
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (Connected)
+                {
+                    throw new InvalidOperationException($"You can't change the password while connected.");
+                }
+
+                _password = value;
+            }
+        }
+
+        public RawIrcClient Raw { get; }
+
+        public string RealName
+        {
+            get => _realName;
+            set
+            {
+                if (Connected)
+                {
+                    throw new InvalidOperationException($"You can't change your real name while connected.");
+                }
+
+                _realName = value;
+            }
+        }
+
+        public async Task ChangeNicknameAsync(string nickname)
+        {
+            if (!Connected)
+            {
+                throw new NotConnectedException();
+            }
+
+            await Raw.SendMessageAsync(new RawMessage("NICK", nickname));
+        }
+
+        /// <summary>
+        /// Connects to an IRC server.
+        /// </summary>
+        /// <param name="host">The hostname of the server you want to connect to.</param>
+        /// <param name="port">The port of the server you want to connect to. If <see cref="null"/>, the default port appropriate for the <paramref name="ssl"/> parameter will be used.</param>
+        /// <param name="ssl">Whether to connect with SSL underlying.</param>
+        public async Task ConnectAsync(string host, int? port = null, bool ssl = false)
+        {
+            if (!port.HasValue)
+            {
+                port = ssl ? 6697 : 6667;
+            }
+
+            await Raw.ConnectAsync(host, port.Value, ssl);
+
+            // https://modern.ircdocs.horse/#connection-registration
+            await Raw.SendMessageAsync(new RawMessage("CAP", "LS", "302"));
+
+            if (Password != null)
+            {
+                await Raw.SendMessageAsync(new RawMessage("PASS", parameters: Password));
+            }
+
+            await Raw.SendMessageAsync(new RawMessage("NICK", parameters: Nickname));
+            // TODO: Use a separate property for username.
+            await Raw.SendMessageAsync(new RawMessage("USER", Nickname, "*", "*", RealName));
+        }
+
+        public void Dispose() => ((IDisposable)Raw).Dispose();
+
+        /// <summary>
+        /// Sends a message to the specified target.
+        /// </summary>
+        /// <param name="target">The channel or user where the message will be sent to. Use # to specify a channel.</param>
+        /// <param name="message">The message to send</param>
+        public async Task SendMessageAsync(string target, string message)
+        {
+            var privMsg = new RawMessage("PRIVMSG", target, message);
+            await Raw.SendMessageAsync(privMsg);
+        }
     }
 }
