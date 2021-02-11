2185e4d6aa59c47a10c75efb8368eed959758dbc
Craftplacer <craftplacer@outlook.de>
2021-02-11 14:37:33 +0100

    Implement hostmask parsing

--- Craftplacer.IRC/Helpers/Utilities.cs
+++ Craftplacer.IRC/Helpers/Utilities.cs
@@ -6,7 +6,13 @@ namespace Craftplacer.IRC.Helpers
 {
     public static class Utilities
     {
-        internal static (string Nickname, string Username, string Host) ExtractHostmask(string hostmask) => throw new NotImplementedException();
+        internal static (string Nickname, string Username, string Host) ExtractHostmask(string hostmask)
+        {
+            var hostSplit = hostmask.Split('@', 2);
+            var host = hostSplit[1];
+            var userSplit = hostSplit[0].Split('!', 2);
+            return (userSplit[0], userSplit[1], host);
+        }
 
         internal static bool SafeSequenceEqual<T>(IEnumerable<T> a, IEnumerable<T> b)
         {
