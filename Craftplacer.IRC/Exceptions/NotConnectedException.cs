c9e9c03674f1d936c799610a150f29aeb6bc43c3
Craftplacer <craftplacer@outlook.de>
2021-02-11 02:13:05 +0100

    Create NotConnectedException.cs

---
+++ Craftplacer.IRC/Exceptions/NotConnectedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Craftplacer.IRC
+{
+    [Serializable]
+    public class NotConnectedException : InvalidOperationException
+    {
+        public NotConnectedException() : base("You can't perform this operation unless you are connected.")
+        {
+        }
+    }
+}
